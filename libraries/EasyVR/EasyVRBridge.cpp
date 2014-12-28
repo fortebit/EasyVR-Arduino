069107cbe234a2dfc92c65f1e7eefa10cfc8d966
ppescher <p.messina@robotechsrl.com>
2014-12-28 12:25:11 +0100

    fixed missing include file for CDC_ENABLED

--- libraries/EasyVR/EasyVRBridge.cpp
+++ libraries/EasyVR/EasyVRBridge.cpp
@@ -11,6 +11,7 @@ file COPYING.txt or at this address: <http://www.opensource.org/licenses/MIT>
 
 #if defined(ARDUINO) && ((ARDUINO >= 106 && ARDUINO < 150) || ARDUINO >= 155)
   #include "Arduino.h"
+  #include "Platform.h"
 #else
   #error "Arduino version not supported. Please update your IDE to the latest version."
 #endif
